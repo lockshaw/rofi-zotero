7501e85a214ce57ae02a1a77c5ba6a2a9797d42b
Matthijs Mars <matthijs.mars97@gmail.com>
2021-12-22 00:15:43 +0100

    Added searchable authors and years besides the title

--- rofi-zotero.py
+++ rofi-zotero.py
@@ -18,9 +18,22 @@ if rofi_theme:
 else:
   theming = []
 
+# Query for getting the Authors
+q_authors = """
+select 
+  items.itemID, creators.creatorID, creators.Lastname, itemCreators.orderIndex
+from 
+  (
+    (creators JOIN itemCreators
+    ON creators.creatorID = itemCreators.creatorID) as t1
+    JOIN items
+    ON t1.itemID = items.itemID
+  )
+"""
 
-PDFS_QUERY = """ 
-    SELECT attachmentInfo.key,  
+# Query for getting the titles
+q_titles = """ 
+    SELECT parentInfo.itemID, attachmentInfo.key,  
            parentItemDataValues.value 
     FROM itemAttachments 
     INNER JOIN items AS attachmentInfo 
@@ -35,13 +48,34 @@ PDFS_QUERY = """
         ON parentItemData.itemID = parentInfo.itemID 
     INNER JOIN itemDataValues as parentItemDataValues 
         ON parentItemDataValues.valueID = parentItemData.valueID 
-    WHERE attachmentItemData.fieldID = 1
-      AND parentItemData.fieldID = 1
+    WHERE 
+    attachmentItemData.fieldID = 1 AND parentItemData.fieldID = 1
       AND (itemAttachments.contentType LIKE '%pdf%'
           OR itemAttachments.contentType LIKE '%djvu%')
-""" 
+"""
 
-def getPDFSet(zotero_dir):
+
+# Query for getting the publishing dates
+q_dates = """ 
+    SELECT parentInfo.itemID, attachmentInfo.key,  
+           parentItemDataValues.value, parentItemData.fieldID
+    FROM itemAttachments 
+    INNER JOIN items AS attachmentInfo 
+        ON attachmentInfo.itemID = itemAttachments.itemID 
+    INNER JOIN itemData as attachmentItemData 
+        ON attachmentItemData.itemID = attachmentInfo.itemID 
+    INNER JOIN itemDataValues as attachmentItemDataValues 
+        ON attachmentItemData.valueID = attachmentItemDataValues.valueID 
+    INNER JOIN items AS parentInfo 
+        ON itemAttachments.parentItemID = parentInfo.itemID 
+    INNER JOIN itemData as parentItemData 
+        ON parentItemData.itemID = parentInfo.itemID 
+    INNER JOIN itemDataValues as parentItemDataValues 
+        ON parentItemDataValues.valueID = parentItemData.valueID 
+    WHERE parentItemData.fieldID=6
+"""
+
+def getPDFSet(zotero_dir, PDFS_QUERY):
   conn = sqlite3.connect(os.path.join(zotero_dir, ZOTERO_SQLITE_FILE))
 
   pdfs = conn.execute(PDFS_QUERY)
@@ -49,9 +83,9 @@ def getPDFSet(zotero_dir):
   pdf_list = []
 
   for pdf in pdfs:
-    if pdf[1] not in seen_pdfs:
-      pdf_list.append((pdf[0], pdf[1]))
-      seen_pdfs.add(pdf[1])
+    # if pdf[1] not in seen_pdfs:
+    pdf_list.append(pdf)
+    seen_pdfs.add(pdf)
 
   conn.close()
 
@@ -82,12 +116,59 @@ parser.add_argument( \
     help="set the Zotero directory")
 
 args = parser.parse_args()
-pdfs = getPDFSet(args.zotero)
+# pdfs = getPDFSet(args.zotero)
 pdfs_str = ""
-labels = list(range(len(pdfs)))
+# labels = list(range(len(pdfs)))
 
-for i, pdf in enumerate(pdfs):
-  pdfs_str += "({}) {}\n".format(labels[i], pdf[1])
+
+a_authors = getPDFSet(args.zotero, q_authors)
+a_titles = getPDFSet(args.zotero, q_titles)
+a_dates = getPDFSet(args.zotero, q_dates)
+
+authors = {}
+id = 1
+author_list = []
+for i in a_authors:
+    if i[0] != id:
+        authors[id] = author_list 
+        id = i[0]
+        author_list = []
+    author_list.append(i[2])
+authors[id] = author_list
+
+
+for i in authors:
+    l = authors[i]
+    if len(l) == 1:
+        authors[i] = l[0].split(",")[0]
+    elif len(l) == 2:
+        authors[i] = l[0].split(",")[0] + " & " + l[1].split(",")[0]
+    elif len(l) >= 3:
+        authors[i] = l[0].split(",")[0] + " et al."
+
+years = {}
+for i in a_dates:
+    if i[0] not in years:
+        years[i[0]] = i[2].split("-")[0]
+
+titles = {}
+keys = {}
+for i in a_titles:
+    if i[0] not in titles:
+        titles[i[0]] = i[2]
+        keys[i[0]] = i[1]
+
+
+pdf_list = []
+for itemID in titles:
+    if itemID in authors and itemID in years:
+        string = authors[itemID] + " " + years[itemID] + " - " + titles[itemID]
+        pdf_list.append((string, keys[itemID]))
+
+labels = list(range(len(pdf_list)))
+
+for i, pdf in enumerate(pdf_list):
+  pdfs_str += "({}) {}\n".format(labels[i], pdf[0])
 
 if args.list:
   print(pdfs_str)
@@ -96,12 +177,11 @@ else:
   rofi = subprocess.run(["rofi", "-threads", "0", "-dmenu", "-i", "-p", "paper"] + theming, \
       capture_output=True, text=True, input=pdfs_str)
   selected_pdf = rofi.stdout.strip()
-  print(selected_pdf)
   if len(selected_pdf) > 0:
     re_result = re.match(r"\((?P<index>\w+)\)", selected_pdf)
     if re_result is not None:
       index = re_result.group("index")
-      key = pdfs[int(index)][0]
+      key = pdf_list[int(index)][1]
       storage_dir = os.path.join(args.zotero, ZOTERO_STORAGE_DIR, key)
       file_to_open = pick_file(os.listdir(storage_dir))
       file_to_open_path = os.path.join(storage_dir, file_to_open)
